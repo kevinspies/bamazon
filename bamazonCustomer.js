dd7c8179cc6cd8de40f7c93659b3baa8f1bb90eb
Kevin Spies <kevin.spies@gmail.com>
2019-07-17 16:18:02 -0400

    select query working perfectly! i think i'll bring inquirer in now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,21 +13,21 @@ var connection = mysql.createConnection({
 
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-});
-
 // connection.connect(function (err) {
 //     if (err) throw err;
 //     console.log("connected as id " + connection.threadId);
-//     afterConnection();
+//     connection.end();
 // });
-// function afterConnection() {
-//     connection.query("SELECT * FROM products", function (err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//         connection.end();
-//     });
-// }
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
