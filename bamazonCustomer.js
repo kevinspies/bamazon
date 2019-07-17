f632e7f8aac557dec3eb138b395598f379c0520c
Kevin Spies <kevin.spies@gmail.com>
2019-07-17 18:21:31 -0400

    working on query's, got columnify working, still about to put in inquirer, also just reading things online

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var columnify = require("columnify");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 
@@ -29,19 +30,29 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         //was trying to make columnify work but putting that on hold
-        // var columns = columnify(data, options);
-        console.log(res);
-        queryAllSongs();
+        // var columns = columnify(res, options);
+        var columns = columnify(res);
+        console.log(columns);
+        // console.log(res);
+        // queryDepartmentProducts();
         connection.end();
     });
 }
-function queryAllSongs() {
+function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
         console.log("-----------------------------------");
+    });
+}
+function queryDepartmentProducts() {
+    var query = connection.query("SELECT * FROM products WHERE department_name=?", ["Clothes"], function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
     });
 }
 
