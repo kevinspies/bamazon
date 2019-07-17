6b919af3ff15bc266548611939edb08e3c70287f
Kevin Spies <kevin.spies@gmail.com>
2019-07-17 17:13:27 -0400

    prettified a bit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var columnify = require("columnify");
 
 var connection = mysql.createConnection({
 
@@ -12,7 +13,7 @@ var connection = mysql.createConnection({
     database: "bamazon"
 
 });
-
+//uncomment if i want to connect and run query at the same time for some reason
 // connection.connect(function (err) {
 //     if (err) throw err;
 //     console.log("connected as id " + connection.threadId);
@@ -27,7 +28,22 @@ connection.connect(function (err) {
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+        //was trying to make columnify work but putting that on hold
+        // var columns = columnify(data, options);
         console.log(res);
+        queryAllSongs();
         connection.end();
     });
 }
+function queryAllSongs() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("-----------------------------------");
+    });
+}
+
+
+
