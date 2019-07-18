9514514035b772b9da1d6c2b817a17b0d10f7470
Kevin Spies <kevin.spies@gmail.com>
2019-07-17 22:16:04 -0400

    commenting and planning.. i need to use recursion with inquirer to keep the prompts going, and just put functions in the right place..

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,8 +24,11 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+
+
+    // afterConnection();
 });
+//maybe this after connection method is superflous?
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -48,6 +51,7 @@ function queryAllProducts() {
     });
 }
 function queryDepartmentProducts() {
+    //i should replace 'Clothes' with some input from inquirer!
     var query = connection.query("SELECT * FROM products WHERE department_name=?", ["Clothes"], function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
@@ -56,5 +60,37 @@ function queryDepartmentProducts() {
     });
 }
 
+//i want a function for querying the item name, so i can call that and tell the user 
+//what item they just bought
+function queryItem() {
+    connection.query("SELECT")
+}
 
+//let's put inquirer here, i think it is a good place for it?*test*, right after we 
+//connect, before we make any queries actually, shouldn't it be in afterConnection()?
+//well now it's here *test*
+//theoretically couldn't i leave it here, connect above, compiler reads through, finally gets 
+//here to inquirer where all the interfacing and everything happens, i am connected
+//so everything is fine.
+inquirer.prompt([
+    {
+        type: "input",
+        message: "ID of product you'd like to buy",
+        name: "item"
+    },
+    {
+        type: "input",
+        message: "how many?",
+        name: "number"
+    }
+]).then(function (input) {
+    // if (inqRes.confirm) {
+    console.log("Great, now let's process your request of... " + input.number + " amount of id#" + input.item);
+    // }
+    // else {
+    //     console.log("\nThat's okay, come again when you are more sure.\n");
+    // }
 
+    //QUERIES GO HERE
+
+});
